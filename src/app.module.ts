aa64e868a52119b5fefa632fa0990b0a9fc7b04b
ironjin <cheoljin1234@naver.com>
2025-07-05 14:05:03 +0900

    [feature/api] - 초기 프로젝트 구성

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entity/user.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'auth',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
